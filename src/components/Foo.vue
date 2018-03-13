eda74e9e4bdcc76a0f6a0e06e09180bcccb458f4
Balint Soos <balint.soos@emarsys.com>
2018-03-13 14:25:41 +0100

    Vuex integration

--- src/components/Foo.vue
+++ src/components/Foo.vue
@@ -1,10 +1,27 @@
 <template>
-    <h2>{{ $t("message.foo") }}</h2>
+    <div>
+        <h2>{{ $t("message.foo") }}</h2>
+        <p>{{ count }}</p>
+        <button v-on:click="increment">
+            Increment
+        </button>
+    </div>
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex';
+
 export default {
-    data: () => ({}),
+    computed: {
+        ...mapState({
+            count: 'count',
+        })
+    },
+    methods: {
+        ...mapMutations([
+            'increment',
+        ])
+    },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+
 import App from './App';
+import store from './store';
 import router from './router';
 import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-const app = new Vue({
+new Vue({
   el: '#app',
+  store,
   router,
   i18n,
   components: { App },
